0937004eeab77b235f3c48665b44a2371995a29b
tharunShiv <tharun.rocky14@gmail.com>
2018-05-18 21:58:43 +0530

    Programmer information added in the about view

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -2,5 +2,19 @@
 
 @section('content')
 
-
+<div class="container">
+    <h2>About DSCE File Manager</h2>
+    <h3>Created By</h3>
+    <div class="programmer">
+        <pre>
+        <b>Tharun</b> 
+        Student at DSCE, Programming YouTuber and Blogger,
+        Web and App Developer,
+        Data Scientist
+        Contact: <i>asktharun@gmail.com</i>
+        YouTube : <a href="https://www.youtube.com/channel/UCDvWreVvvAwyoVHXG7QnqRg">Click here to view the channel</a>
+        Blog : <a href="https://tharunshiv.ml">tharunshiv.ml</a> 
+        </pre>
+    </div>
+</div>
 @endsection
