ec283e6f04a25eda26844eafb515728eea2d8384
tharunShiv <tharun.rocky14@gmail.com>
2018-05-19 22:31:26 +0530

    border changes to the image with radius and the color

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -7,7 +7,7 @@
     <h3>Created By</h3>
     <div class="row">
         <div class="col-sm-12 col-md-4">
-            <img src="images/tharun.jpeg" class="img-rounded img-responsive" width="200px;" style="margin-left:20%"/>
+            <img src="images/tharun.jpeg" class="" width="200px;" style="margin-left:20%;border:1px solid #343a40;border-radius:5%;"/>
         </div>
         <div class="col-sm-12 col-md-4">
                 <div class="programmer" style="border-left:5px dashed #343a40;border-radius:10%;">
