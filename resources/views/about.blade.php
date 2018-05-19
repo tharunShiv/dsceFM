b51143306e327630ad4f9b6ab17f61699775ae9d
tharunShiv <tharun.rocky14@gmail.com>
2018-05-19 22:23:38 +0530

    responsive image set in the view

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -3,18 +3,26 @@
 @section('content')
 
 <div class="container">
-    <h2>About DSCE File Manager</h2>
+    <h2>About DSCE File Manager</h2><br>
     <h3>Created By</h3>
-    <div class="programmer">
-        <pre>
-        <b>Tharun</b> 
-        Student at DSCE, Programming YouTuber and Blogger,
-        Web and App Developer,
-        Data Scientist
-        Contact: <i>asktharun@gmail.com</i>
-        YouTube : <a href="https://www.youtube.com/channel/UCDvWreVvvAwyoVHXG7QnqRg">Click here to view the channel</a>
-        Blog : <a href="https://tharunshiv.ml">tharunshiv.ml</a> 
-        </pre>
+    <div class="row">
+        <div class="col-sm-12 col-md-4">
+            <img src="images/tharun.jpeg" class="img-rounded img-responsive" width="200px;" style="margin-left:20%"/>
+        </div>
+        <div class="col-sm-12 col-md-4">
+                <div class="programmer">
+<pre>
+<b>Tharun</b> 
+Student at DSCE, Programming YouTuber and Blogger,
+Web and App Developer,
+Data Scientist
+Contact: <i>asktharun@gmail.com</i>
+YouTube : <a href="https://www.youtube.com/channel/UCDvWreVvvAwyoVHXG7QnqRg">Click here to view the channel</a>
+Blog : <a href="https://tharunshiv.ml">tharunshiv.ml</a> 
+</pre>
+                </div>
+        </div>
     </div>
+    
 </div>
 @endsection
