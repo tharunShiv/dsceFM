94148773ff4fcac753a5606d5df7c2accfe595d9
tharunShiv <tharun.rocky14@gmail.com>
2018-05-24 22:07:00 +0530

    Removed the main class to decrease margin

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -71,12 +71,12 @@
             </div>
         </nav>
 
-        <main class="py-4">
+        
             <div class="container">
                 @include('inc.messages')
             </div>
             @yield('content')
-        </main>
+        
     </div>
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.0/umd/popper.min.js"></script>
